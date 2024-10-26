0ab382a1eb9d54fa6b5e775db1140e8254a86f3e
sofiia-0 <ana.sofiia02@gmail.com>
2024-10-26 14:10:42 -0600

    Initial Commit

---
+++ S3PracticaLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S3PracticaLab
+//
+//  Created by LABORATORIO MAC UAM on 26/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
